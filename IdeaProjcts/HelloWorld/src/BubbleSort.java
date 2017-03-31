6c8e912b18d668d0e9b8206f4e0a1072cf4ee3e6
Alexey <orange_85@mail.ru>
2017-03-31 21:58:51 +0700

    пузырьковая сортировка на обычном масиве ( прерывание щечика после прохода без замены с помощью boolean)

--- IdeaProjcts/HelloWorld/src/BubbleSort.java
+++ IdeaProjcts/HelloWorld/src/BubbleSort.java
@@ -4,13 +4,16 @@
  * Created by Alexey on 02.03.2017.
  */
 public class BubbleSort {
-    public static int[] bubbleSort(int[] arr) {                   //функция сортировки пузырьком
+    public static int[] bubbleSort(int[] arr) {      //функция сортировки пузырьком
+        boolean branch = false;
         for (int i = arr.length - 1; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int t = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = t;
+                } else if (branch == true) {
+                    return arr;
                 }
             }
         }
